b3469a1485b1c94003190f8fe74d544ca688425d
ingalls <nick@ingalls.ca>
2020-03-31 10:14:07 -0700

    Refactor creds & add linting

--- cli.js
+++ cli.js
@@ -1,14 +1,12 @@
 #! /usr/bin/env node
 
+'use strict';
+
 const fs = require('fs');
 const artifacts = require('./lib/artifacts');
 const schema = require('./data/cf_schema.json');
 const cf = require('@mapbox/cfn-config');
-const AWS = require('aws-sdk');
 const friend = require('@mapbox/cloudfriend');
-const path = require('path');
-const prompt = require('prompt');
-const cp = require('child_process');
 
 const Credentials = require('./lib/creds');
 
@@ -18,7 +16,7 @@ const mode = {
     list: require('./lib/list'),
     init: require('./lib/init'),
     info: require('./lib/info')
-}
+};
 
 const argv = require('minimist')(process.argv, {
     boolean: ['help', 'version'],
@@ -37,7 +35,7 @@ if (!argv._[2] || argv._[2] === 'help' || (!argv._[2] && argv.help)) {
     console.log();
     console.log('Usage: deploy <command> [--profile <name>] [--template <path>]');
     console.log('              [--version] [--help]');
-    console.log()
+    console.log();
     console.log('Create, manage and delete Cloudformation Resouces from the CLI');
     console.log();
     console.log('Subcommands:');
@@ -52,7 +50,7 @@ if (!argv._[2] || argv._[2] === 'help' || (!argv._[2] && argv.help)) {
     console.log('[options]:');
     console.log('    --profile <name>        If there are multiple AWS profiles set up, the profile to deploy');
     console.log('                              with must be defined either via a .deploy file or via this flag');
-    console.log('    --template <path>       The master template should be found at "cloudformation/<repo-name>.template.js(on)"')
+    console.log('    --template <path>       The master template should be found at "cloudformation/<repo-name>.template.js(on)"');
     console.log('                              if the project has multiple CF Templates, they can be deployed by specifying');
     console.log('                              their location with this flag. The stack will be named:');
     console.log('                              <repo>-<stack name>-<template name>');
@@ -77,12 +75,12 @@ if (command === 'create' && argv.help) {
 } else if (command === 'update' && argv.help) {
     console.log();
     console.log('Usage: deploy update <STACK>');
-    console.log()
+    console.log();
     return;
 } else if (command === 'delete' && argv.help) {
     console.log();
     console.log('Usage: deploy delete <STACK>');
-    console.log()
+    console.log();
     return;
 } else if (mode[command] && argv.help) {
     mode[command].help();
@@ -96,70 +94,66 @@ if (['create', 'update', 'delete'].indexOf(command) > -1) {
     if (!argv._[3]) return console.error(`Stack name required: run deploy ${command} --help`);
     const stack = argv._[3];
 
-    Credentials.load(argv, {}, (err, creds) => {
-        if (err) throw err;
+    const creds = new Credentials(argv, {});
 
-        cf.preauth(creds);
+    cf.preauth(creds);
 
-        const cf_cmd = cf.commands({
-            name: creds.repo,
-            region: creds.region,
-            configBucket: `cfn-config-active-${creds.accountId}-${creds.region}`,
-            templateBucket: `cfn-config-templates-${creds.accountId}-${creds.region}`
-        });
+    const cf_cmd = cf.commands({
+        name: creds.repo,
+        region: creds.region,
+        configBucket: `cfn-config-active-${creds.accountId}-${creds.region}`,
+        templateBucket: `cfn-config-templates-${creds.accountId}-${creds.region}`
+    });
 
-        friend.build(creds.template).then(template => {
-            cf_path = `/tmp/${hash()}.json`;
+    friend.build(creds.template).then((template) => {
+        const cf_path = `/tmp/${hash()}.json`;
 
-            template = tagger(template, creds.dotdeploy.tags);
+        template = tagger(template, creds.dotdeploy.tags);
 
-            fs.writeFileSync(cf_path, JSON.stringify(template, null, 4));
+        fs.writeFileSync(cf_path, JSON.stringify(template, null, 4));
 
-            if (command === 'create') {
-                artifacts(creds, (err) => {
-                    if (err) return console.error(`Artifacts Check Failed: ${err.message}`);
+        if (command === 'create') {
+            artifacts(creds, (err) => {
+                if (err) return console.error(`Artifacts Check Failed: ${err.message}`);
 
-                    cf_cmd.create(creds.subname + stack, cf_path, {
-                        parameters: {
-                            GitSha: creds.sha
-                        }
-                    }, (err) => {
-                        if (err) return console.error(`Create failed: ${err.message}`);
-                        fs.unlinkSync(cf_path);
-                    });
-                });
-            } else if (command === 'update') {
-                artifacts(creds, (err) => {
-                    if (err) return console.error(`Artifacts Check Failed: ${err.message}`);
-
-                    cf_cmd.update(creds.subname + stack, cf_path, {
-                        parameters: {
-                            GitSha: creds.sha
-                        }
-                    }, (err) => {
-                        if (err) return console.error(`Update failed: ${err.message}`);
-                        fs.unlinkSync(cf_path);
-                    });
-                });
-            } else if (command === 'delete') {
-                cf_cmd.delete(creds.subname + stack, (err) => {
-                    if (err) return console.error(`Delete failed: ${err.message}`);
+                cf_cmd.create(creds.subname + stack, cf_path, {
+                    parameters: {
+                        GitSha: creds.sha
+                    }
+                }, (err) => {
+                    if (err) return console.error(`Create failed: ${err.message}`);
                     fs.unlinkSync(cf_path);
                 });
-            }
-        });
-    })
+            });
+        } else if (command === 'update') {
+            artifacts(creds, (err) => {
+                if (err) return console.error(`Artifacts Check Failed: ${err.message}`);
+
+                cf_cmd.update(creds.subname + stack, cf_path, {
+                    parameters: {
+                        GitSha: creds.sha
+                    }
+                }, (err) => {
+                    if (err) return console.error(`Update failed: ${err.message}`);
+                    fs.unlinkSync(cf_path);
+                });
+            });
+        } else if (command === 'delete') {
+            cf_cmd.delete(creds.subname + stack, (err) => {
+                if (err) return console.error(`Delete failed: ${err.message}`);
+                fs.unlinkSync(cf_path);
+            });
+        }
+    });
 } else if (mode[command]) {
     if (['init'].includes(command)) {
         mode[command].main(process.argv);
     } else {
-        Credentials.load(argv, {
+        const creds = new Credentials(argv, {
             template: false
-        }, (err, creds) => {
-            if (err) throw err;
-
-            mode[command].main(creds, process.argv);
         });
+
+        mode[command].main(creds, process.argv);
     }
 } else {
     console.error('Subcommand not found!');
@@ -183,7 +177,7 @@ function tagger(template, tags) {
 
         if (!template.Resources[name].Properties) {
             template.Resources[name].Properties = {};
-        };
+        }
 
         if (!template.Resources[name].Properties.Tags) {
             template.Resources[name].Properties.Tags = [];
@@ -209,5 +203,5 @@ function tagger(template, tags) {
 }
 
 function hash() {
-     return Math.random().toString(36).substring(2, 15);
+    return Math.random().toString(36).substring(2, 15);
 }
