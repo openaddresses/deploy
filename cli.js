345896452354b8add7b496f1c0337bd86754be07
ingalls <nick@ingalls.ca>
2020-04-15 11:45:54 -0700

    Add stack name support

--- cli.js
+++ cli.js
@@ -20,7 +20,7 @@ const mode = {
 
 const argv = require('minimist')(process.argv, {
     boolean: ['help', 'version'],
-    string: ['profile', 'template'],
+    string: ['profile', 'template', 'name'],
     alias: {
         version: 'v'
     }
@@ -91,8 +91,7 @@ if (command === 'create' && argv.help) {
 }
 
 if (['create', 'update', 'delete'].indexOf(command) > -1) {
-    if (!argv._[3]) return console.error(`Stack name required: run deploy ${command} --help`);
-    const stack = argv._[3];
+    if (!argv._[3] && !argv.name) return console.error(`Stack name required: run deploy ${command} --help`);
 
     const creds = new Credentials(argv, {});
 
@@ -116,7 +115,7 @@ if (['create', 'update', 'delete'].indexOf(command) > -1) {
             artifacts(creds, (err) => {
                 if (err) return console.error(`Artifacts Check Failed: ${err.message}`);
 
-                cf_cmd.create(creds.subname + stack, cf_path, {
+                cf_cmd.create(creds.name, cf_path, {
                     parameters: {
                         GitSha: creds.sha
                     }
@@ -129,7 +128,7 @@ if (['create', 'update', 'delete'].indexOf(command) > -1) {
             artifacts(creds, (err) => {
                 if (err) return console.error(`Artifacts Check Failed: ${err.message}`);
 
-                cf_cmd.update(creds.subname + stack, cf_path, {
+                cf_cmd.update(creds.name, cf_path, {
                     parameters: {
                         GitSha: creds.sha
                     }
@@ -139,7 +138,7 @@ if (['create', 'update', 'delete'].indexOf(command) > -1) {
                 });
             });
         } else if (command === 'delete') {
-            cf_cmd.delete(creds.subname + stack, (err) => {
+            cf_cmd.delete(creds.name, (err) => {
                 if (err) return console.error(`Delete failed: ${err.message}`);
                 fs.unlinkSync(cf_path);
             });
@@ -175,6 +174,11 @@ function tagger(template, tags) {
             || !schema.ResourceTypes[template.Resources[name].Type].Properties.Tags
         ) continue;
 
+        const special = [];
+        if (schema.ResourceTypes[template.Resources[name].Type].Properties.Tags.ItemType === 'TagProperty') {
+            special.push(["PropagateAtLaunch", true])
+        }
+
         if (!template.Resources[name].Properties) {
             template.Resources[name].Properties = {};
         }
@@ -197,6 +201,16 @@ function tagger(template, tags) {
                 template.Resources[name].Properties.Tags.push(oTag);
             }
         }
+
+        template.Resources[name].Properties.Tags = template.Resources[name].Properties.Tags.map((tag) => {
+            tag = JSON.parse(JSON.stringify(tag));
+
+            special.forEach((s) => {
+                tag[s[0]] = s[1];
+            });
+
+            return tag;
+        });
     }
 
     return template;
